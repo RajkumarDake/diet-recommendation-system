6d129acac1d9343b35d4f582676d8e9467ae12b1
Dake Rajkumar <108937933+RajkumarDake@users.noreply.github.com>
2025-03-31 12:34:56 +0530

    Update Footer.js
    updated the name

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <div className="flex items-center justify-evenly  text-white flex-wrap px-3 py-4 width:100%">
       <p className="text-sm text-center my-2">
-        Designed and Developed by Gayatri
+        Designed and Developed by Rajkumar
       </p>
     </div>
   );
